5b3c510ffbb5b5fa4c4e36a513144ca1ada3debe
David Diaz <david.diaz@liquidlabs.de>
2015-11-27 15:44:27 +0100

    minor bug fix

--- git-prune.plugin.zsh
+++ git-prune.plugin.zsh
@@ -1,11 +1,11 @@
 
 gprune() {
     if [[ "$1" == "-l" ]]; then
-        git branch --merged | grep -v "$1" | grep -v "master" | xargs git branch -d &&
+        git branch --merged | grep -v "$2" | grep -v "master" | xargs git branch -d &&
         git branch;
     elif [[  "$1" == "-r" ]]; then
         git fetch --prune &&
-        git branch -r --merged | grep -v "$1" | grep -v "master" | grep -v "HEAD" | sed -e 's/origin\//:/' | xargs git push origin &&
+        git branch -r --merged | grep -v "$2" | grep -v "master" | grep -v "HEAD" | sed -e 's/origin\//:/' | xargs git push origin &&
         git branch -a;
     else
         git fetch --prune &&
