166ae74154bc49ed7382528e3d307673fefe1a07
David Diaz <david.diaz@liquidlabs.de>
2016-03-11 11:25:39 +0100

    Fixed problems with remote branches deletion

--- git-prune.plugin.zsh
+++ git-prune.plugin.zsh
@@ -1,8 +1,16 @@
 gprune() {
+  __prune_remote_branches() {
+    git push origin `git branch -r --merged "$1" | grep -v "HEAD" | grep -v '/release' | grep -v '/master$' | grep -v "/staging$" | grep -v "/develop$" | grep -v "/$1$" | sed 's/origin\//:/g' | tr -d '\n'`
+  }
+
+  __prune_local_branches() {
+    git branch -d `git branch --merged "$1" | grep -v 'master$'  | grep -v 'release' | grep -v "develop$" | grep -v "staging$" | grep -v "$1$" | sed 's/origin\///g' | tr -d '\n'`
+  }
+
   __print_remote_branches() {
     echo "Fetching branches..."
     git fetch --prune
-    __remote_branches=$(git branch -r --merged| grep -v '/release'| grep -v "HEAD" | grep -v '/master$' | grep -v "/$1$")
+    __remote_branches=$(git branch -r --merged "$1" | grep -v "HEAD" | grep -v '/release' | grep -v '/master$' | grep -v "/staging$" | grep -v "/develop$" | grep -v "/$1$")
     if [[ -n "$__remote_branches" ]]; then
       echo "These REMOTE branches will be removed:"
       echo "$__remote_branches"
@@ -10,19 +18,11 @@ gprune() {
   }
 
   __print_local_branches() {
-    __local_branches=$(git branch --merged | grep -v 'release' | grep -v 'master$' | grep -v "$1$")
+    __local_branches=$(git branch --merged "$1" | grep -v 'master$'  | grep -v 'release' | grep -v "develop$" | grep -v "staging$" | grep -v "$1$")
     if [[ -n "$__local_branches" ]]; then
       echo "These LOCAL branches will be removed:"
       echo "$__local_branches"
     fi
-  }
-
-  __prune_remote_branches() {
-    git push origin `git branch -r --merged | grep -v '/master$' | grep -v '/release'| grep -v "HEAD" | grep -v "/$1$" | grep -v "develop" |grep -v "staging"|sed 's/origin\//:/g' | tr -d '\n'`
-  }
-
-  __prune_local_branches() {
-    git branch -d `git branch --merged | grep -v 'master$'  | grep -v 'release' |grep -v "develop" |grep -v "staging"| grep -v "$1$" | sed 's/origin\///g' | tr -d '\n'`
   }
 
   isRemote=false
@@ -50,8 +50,8 @@ gprune() {
   if [[ -z "$branch_to_compare" ]]; then
     echo "The branch name is invalid"
   else
-    remote_branches=$(git branch -r --merged| grep -v '/release'| grep -v "HEAD" | grep -v '/master$' | grep -v "/$branch_to_compare$")
-    local_branches=$(git branch --merged | grep -v 'release' | grep -v 'master$' | grep -v "$branch_to_compare$")
+    remote_branches=$(git branch -r --merged "$branch_to_compare" | grep -v "HEAD" | grep -v '/release' | grep -v '/master$' | grep -v "/staging$" | grep -v "/develop$" | grep -v "/$branch_to_compare$")
+    local_branches=$(git branch --merged "$branch_to_compare" | grep -v 'master$'  | grep -v 'release' | grep -v "develop$" | grep -v "staging$" | grep -v "$branch_to_compare$")
 
     echo "Current branch: $branch_to_compare"
     if ([ -z "$remote_branches" ] && $isRemote ]) || ([ -z "$local_branches" ] && $isLocal ]) || ([ -z "$remote_branches" ] && [ -z "$local_branches" ]); then
